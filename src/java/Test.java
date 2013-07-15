55ee8e9646e64fd2ecd934432f997eff7f11ac91
David Massart <david@zettadata.net>
2013-07-15 17:47:41 +0300

    minor change

--- src/java/Test.java
+++ src/java/Test.java
@@ -7,12 +7,13 @@ import net.zettadata.simpleparser.SimpleMetadataFactory;
 
 public class Test {
 
-	public static final String ROOT_FOLDER = "/Users/dmssrt/home/projects/agroKnow/agroService" ; 
-	public static final String LOM_FOLDER = ROOT_FOLDER + File.separator + "simpleParser" + File.separator + "metadata" + File.separator + "LOMs" + File.separator ;
-	public static final String AKIF_FOLDER = ROOT_FOLDER + File.separator + "simpleParser" + File.separator + "metadata" + File.separator + "AKIFs" + File.separator ;
-	public static final String NSDL_FOLDER = ROOT_FOLDER + File.separator + "simpleParser" + File.separator + "metadata" + File.separator + "NSDLs" + File.separator ;
-	public static final String DC_FOLDER = ROOT_FOLDER + File.separator + "simpleParser" + File.separator + "metadata" + File.separator + "DCs" + File.separator ;
-	public static final String MODS_FOLDER = ROOT_FOLDER + File.separator + "simpleParser" + File.separator + "metadata" + File.separator + "MODSs" + File.separator ;
+	public static final String ROOT_FOLDER = "/Users/dmssrt/home/projects/agroKnow/code/" ; 
+	public static final String LOM_FOLDER = ROOT_FOLDER + File.separator + "simpleParse" + File.separator + "metadata" + File.separator + "LOMs" + File.separator ;
+	public static final String AKIF_FOLDER = ROOT_FOLDER + File.separator + "simpleParse" + File.separator + "metadata" + File.separator + "AKIFs" + File.separator ;
+	public static final String NSDL_FOLDER = ROOT_FOLDER + File.separator + "simpleParse" + File.separator + "metadata" + File.separator + "NSDLs" + File.separator ;
+	public static final String DC_FOLDER = ROOT_FOLDER + File.separator + "simpleParse" + File.separator + "metadata" + File.separator + "DCs" + File.separator ;
+	public static final String MODS_FOLDER = ROOT_FOLDER + File.separator + "simpleParse" + File.separator + "metadata" + File.separator + "MODSs" + File.separator ;
+	public static final String AGRIFS_FOLDER = ROOT_FOLDER + File.separator + "simpleParse" + File.separator + "metadata" + File.separator + "AGRIFs" + File.separator ;
 	
 	public static void main( String[] args )
 	{
@@ -21,8 +22,8 @@ public class Test {
 		SimpleMetadata sm ;
 		try 
 		{
-			sm = SimpleMetadataFactory.getSimpleMetadata( SimpleMetadataFactory.MODS ) ;
-			sm.load( MODS_FOLDER + "1.xml" ) ;
+			sm = SimpleMetadataFactory.getSimpleMetadata( SimpleMetadataFactory.AGRIF ) ;
+			sm.load( AGRIFS_FOLDER + "0.json" ) ;
 			System.out.println( sm.toString() ) ;
 		}
 		catch (ParserException e) 
